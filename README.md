cb0b7472a8cb6c789a566b91074818bb09d660b7
Abdoul karim maiga <maiga.projets@gmail.com>
2022-06-28 16:17:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_livraison_restoration
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_livraison_restoration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_livraison_restoration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
