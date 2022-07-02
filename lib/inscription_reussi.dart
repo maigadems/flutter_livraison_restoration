d7cf6d1f48b6b7974f1ceb95d0e536d05ae3f919
meissa-niang <niangmeissa1@gmail.com>
2022-07-02 09:34:26 +0000

    inscription_reussi.dart

---
+++ lib/inscription_reussi.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_livraison_restoration/connection.dart';
+import 'package:flutter_livraison_restoration/inscription.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class InscriptionReussi extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    var size = MediaQuery.of(context).size;
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(10),
+        height: size.height,
+        width: size.width,
+        alignment: Alignment.center,
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.transparent,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor:Color.fromRGBO(252, 14, 14, 1),
+                      radius: 100,
+                      child: Icon(Icons.check_rounded , color: Colors.white, size: size.width/4,),
+                    ),
+                    Divider(color: Colors.transparent),
+                    Text.rich(
+                      TextSpan(
+                        text: 'Congrats !\n',
+                        style: GoogleFonts.viga(
+                            textStyle: TextStyle(
+                              color: Color.fromRGBO(252, 14, 14, 1),
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            )
+                        ),
+                        children: [
+                          TextSpan(
+                            text:'Inscription Reussi',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              FlatButton(
+                onPressed:() {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (BuildContext ctx) {
+                        return Connection();
+                      }));
+                },
+                color: Color.fromRGBO(252, 14, 14, 1),
+                child:Padding(
+                  padding: EdgeInsets.only(top: 18,bottom: 18),
+                  child: Text("S'authentifier",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                    ),
+                  ),
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ) ,
+    );
+  }
+}
